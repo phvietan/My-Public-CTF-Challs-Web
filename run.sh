8597da864516d61257621200d3e15a9ac1351c80
phvietan <phvietan@gmail.com>
2023-03-27 01:40:07 +0700

    fix port

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ sudo docker stop public-ctf-web
 sudo docker rm public-ctf-web
 
 sudo docker build -t public-ctf-web .
-sudo docker run --name public-ctf-web -dp 0.0.0.0:1212:80 --restart always -t public-ctf-web
+sudo docker run --name public-ctf-web -dp 127.0.0.1:1212:80 --restart always -t public-ctf-web
 
